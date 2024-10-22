3b57a7012ea0cca2577c5808bf208237d04a4a79
Janet Awino <janetkeyo@gmail.com>
2024-10-23 00:30:46 +0300

    database migration

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Product extends Model
 {
     use HasFactory;
+    public function Sluggable():array
+    {
+        return 
+        [
+            'slug'=>
+            [
+                'source'=>'name'
+            ]
+
+            ];
+    }
 }
